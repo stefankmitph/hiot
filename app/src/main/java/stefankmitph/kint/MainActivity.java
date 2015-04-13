fa1f7a913bc8f61ef01a45eb19a102daf506cb78
stephank <kumpitsch.stephan@gmx.at>
2015-04-13 23:16:07 +0200

    [ADD] DataBaseHelper [CHG] kint.iml

--- app/src/main/java/stefankmitph/kint/MainActivity.java
+++ app/src/main/java/stefankmitph/kint/MainActivity.java
@@ -15,14 +15,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import stefankmitph.model.DataBaseHelper;
 import stefankmitph.model.SQLiteHelper;
 
 
