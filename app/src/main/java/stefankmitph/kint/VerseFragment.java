bc4763a52b5db5f050da6b9ac38ea37f5b84f403
stephank <kumpitsch.stephan@gmx.at>
2015-04-15 23:37:33 +0200

    [CHG] ProgressFragment... async loading of fragments

--- app/src/main/java/stefankmitph/kint/VerseFragment.java
+++ app/src/main/java/stefankmitph/kint/VerseFragment.java
@@ -1,7 +1,6 @@
 package stefankmitph.kint;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -10,6 +9,7 @@ import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,7 +27,7 @@ import stefankmitph.model.Word;
 /**
  * Created by KumpitschS on 14.04.2015.
  */
-public class VerseFragment extends ProgressFragment {
+public class VerseFragment extends Fragment {
 
     private String book;
     private int chapter;
@@ -42,23 +42,18 @@ public class VerseFragment extends ProgressFragment {
     private View contentView;
 
     private Handler mHandler;
-    private Runnable mShowContentRunnable = new Runnable() {
 
-        @Override
-        public void run() {
-            setContentShown(true);
-        }
 
-    };
+    private boolean isShown = false;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         // Setup content view
-        setContentView(contentView);
+        //setContentView(contentView);
         // Setup text for empty content
-        setEmptyText(R.string.empty);
-        obtainData();
+        //setEmptyText(R.string.empty);
+        //obtainData();
     }
 
     public static VerseFragment newInstance(String book, int chapter, int verse) {
@@ -78,8 +73,6 @@ public class VerseFragment extends ProgressFragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
 
-        //bar = (ProgressBar) activity.findViewById(R.id.progressBar);
-
         this.typeface = Typeface.createFromAsset(activity.getAssets(), "fonts/palab.ttf");
 
         try
@@ -94,26 +87,27 @@ public class VerseFragment extends ProgressFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         book = getArguments().getString("book");
         chapter = getArguments().getInt("chapter");
         verse = getArguments().getInt("verse");
 
         database = provider.getDatabase();
-
         navigator = new BookNavigator(database);
     }
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
+        if(isVisibleToUser) {
+            isShown = true;
+        }
     }
 
     private void obtainData() {
         // Show indeterminate progress
-        setContentShown(false);
+        //setContentShown(false);
 
         mHandler = new Handler();
-        mHandler.postDelayed(mShowContentRunnable, 3000);
+
     }
 
     @Override
@@ -122,15 +116,9 @@ public class VerseFragment extends ProgressFragment {
 
         contentView = inflater.inflate(R.layout.activity_fragment, null);
 
-
         final ViewGroup c = container;
 
         AsyncTask<Object, Void, Word[]> loadTask = new AsyncTask<Object, Void, Word[]>() {
-
-            @Override
-            protected void onPreExecute(){
-            //    bar.setVisibility(View.VISIBLE);
-            }
 
             @Override
             protected Word[] doInBackground(Object... params) {
@@ -141,14 +129,15 @@ public class VerseFragment extends ProgressFragment {
             protected void onPostExecute(Word[] result) {
                 words = result;
                 for(Word word : words) {
-                    TextView textViewStrongs = (TextView) c.findViewWithTag("textViewStrongs" + (word.getWord_nr() - 1));
+                    TextView textViewStrongs = (TextView) contentView.findViewWithTag("textViewStrongs" + (word.getWord_nr() - 1));
                     textViewStrongs.setText(word.getStrongs());
 
-                    TextView textViewWord = (TextView) c.findViewWithTag("textViewWord" + (word.getWord_nr() - 1));
+                    TextView textViewWord = (TextView) contentView.findViewWithTag("textViewWord" + (word.getWord_nr() - 1));
                     textViewWord.setText(word.getWord());
                 }
-                //bar.setVisibility(View.GONE);
-                setContentShown(true);
+
+                //if(isShown)
+                    //setContentShown(true);
             }
         };
 
@@ -164,25 +153,33 @@ public class VerseFragment extends ProgressFragment {
 
         int count =  navigator.getVerseCount(book, chapter);
         for(int i = 0; i < count; i++) {
-            //Word word = words[i];
 
-            LinearLayout linearLayout = new LinearLayout(context);
-            linearLayout.setPadding(10, 20, 10, 10);
-            linearLayout.setOrientation(LinearLayout.VERTICAL);
-            linearLayout.setLayoutParams(
-                    new LinearLayout.LayoutParams(
-                            LinearLayout.LayoutParams.WRAP_CONTENT,
-                            LinearLayout.LayoutParams.WRAP_CONTENT));
+            LinearLayout linearLayout = getLayout(context, i);
 
-            final TextView textViewStrongs = new TextView(context);
-            textViewStrongs.setTag("textViewStrongs" + i);
-            //textViewStrongs.setText(word.getStrongs());
-            textViewStrongs.setTextSize(10.0f);
-            textViewStrongs.setTextColor(Color.rgb(0, 146, 242));
-            //final SQLiteDatabase finalDatabase = database;
-            textViewStrongs.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+            layout.addView(linearLayout);
+        }
+        ((ViewGroup)contentView).addView(layout);
+        return contentView; // super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    private LinearLayout getLayout(Context context, int index) {
+        LinearLayout linearLayout = new LinearLayout(context);
+        linearLayout.setPadding(10, 20, 10, 10);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setLayoutParams(
+                new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        final TextView textViewStrongs = new TextView(context);
+        textViewStrongs.setTag("textViewStrongs" + index);
+        //textViewStrongs.setText(word.getStrongs());
+        textViewStrongs.setTextSize(10.0f);
+        textViewStrongs.setTextColor(Color.rgb(0, 146, 242));
+        //final SQLiteDatabase finalDatabase = database;
+        textViewStrongs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                     /*String text = textViewStrongs.getText().toString();
                     String[] parts = text.split("\\&");
                     String strongs1 = "";
@@ -196,46 +193,30 @@ public class VerseFragment extends ProgressFragment {
 
                     Toast toast = Toast.makeText(v.getContext(), strongs1, Toast.LENGTH_LONG);
                     toast.show();*/
-                }
-            });
-
-            TextView textViewWord = new TextView(context);
-            //textViewWord.setText(word.getWord());
-            textViewWord.setTypeface(typeface);
-            textViewWord.setTextSize(16.0f);
-            textViewWord.setTag("textViewWord" + i);
-
-            TextView textViewConcordance = new TextView(context);
-            //textViewConcordance.setText(word.getConcordance());
-            textViewConcordance.setTextColor(Color.rgb(213, 85, 0));
-            textViewConcordance.setTextSize(14.0f);
-
-            TextView textViewFunctional = new TextView(context);
-            //textViewFunctional.setText(word.getFunctional());
-            textViewFunctional.setTextSize(10.0f);
-            textViewFunctional.setTextColor(Color.rgb(0, 146, 242));
-
-            linearLayout.addView(textViewStrongs);
-            linearLayout.addView(textViewWord);
-            linearLayout.addView(textViewConcordance);
-            linearLayout.addView(textViewFunctional);
-
-            layout.addView(linearLayout);
-        }
-        ViewGroup viewGroup = (ViewGroup)contentView.findViewById(R.id.content_container);
-        viewGroup.addView(layout);
-        return contentView;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        mHandler.removeCallbacks(mShowContentRunnable);
-        if (contentView != null) {
-            ViewGroup parentViewGroup = (ViewGroup) contentView.getParent();
-            if (parentViewGroup != null) {
-                parentViewGroup.removeAllViews();
             }
-        }
+        });
+
+        TextView textViewWord = new TextView(context);
+        //textViewWord.setText(word.getWord());
+        textViewWord.setTypeface(typeface);
+        textViewWord.setTextSize(16.0f);
+        textViewWord.setTag("textViewWord" + index);
+
+        TextView textViewConcordance = new TextView(context);
+        //textViewConcordance.setText(word.getConcordance());
+        textViewConcordance.setTextColor(Color.rgb(213, 85, 0));
+        textViewConcordance.setTextSize(14.0f);
+
+        TextView textViewFunctional = new TextView(context);
+        //textViewFunctional.setText(word.getFunctional());
+        textViewFunctional.setTextSize(10.0f);
+        textViewFunctional.setTextColor(Color.rgb(0, 146, 242));
+
+        linearLayout.addView(textViewStrongs);
+        linearLayout.addView(textViewWord);
+        linearLayout.addView(textViewConcordance);
+        linearLayout.addView(textViewFunctional);
+
+        return linearLayout;
     }
 }
